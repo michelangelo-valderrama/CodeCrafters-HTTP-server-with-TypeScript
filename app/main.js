a54b5ff6513296b9f24a0c6e7e4fc9f49945542a
michelangelo-valderrama <michelangelovalderrama@gmail.com>
2023-11-03 21:14:28 +0100

    pass 2nd stage

--- app/main.js
+++ app/main.js
@@ -1,8 +1,25 @@
 const net = require("node:net")
 
+function parseRequest(rawRequest) {
+  const requestStr = rawRequest.toString()
+  const split = requestStr.split(/\r?\n/)
+  const startLine = split[0].split(" ")
+  return {
+    method: startLine[0],
+    path: startLine[1],
+    version: startLine[2],
+  }
+}
+
 const server = net.createServer((socket) => {
-  socket.on("data", () => {
-    socket.write("HTTP/1.1 200 OK\r\n\r\n")
+  console.log(`[client] connected.`)
+  socket.on("data", (data) => {
+    const { path } = parseRequest(data)
+    if (path === "/") {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n")
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
+    }
     socket.end()
     server.close()
   })
