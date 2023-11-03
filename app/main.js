652c6822b8b7235de8ddda4fe85c6fcf8823b684
michelangelo-valderrama <michelangelovalderrama@gmail.com>
2023-11-03 20:46:35 +0100

    pass 2nd stage

--- app/main.js
+++ app/main.js
@@ -1,8 +1,12 @@
 const net = require("node:net")
 
 const server = net.createServer((socket) => {
-  socket.on("close", () => {
+  socket.on("data", () => {
     socket.write("HTTP/1.1 200 OK\r\n\r\n")
+    socket.end()
+    server.close()
+  })
+  socket.on("close", () => {
     socket.end()
     server.close()
   })
